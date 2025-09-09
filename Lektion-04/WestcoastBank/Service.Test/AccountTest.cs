f61281ffe63add5d5cec6bdfdc662b2e1b17c05c
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2025-09-09 13:30:47 +0200

    Updated Lesson 4

--- Lektion-04/WestcoastBank/Service.Test/AccountTest.cs
+++ Lektion-04/WestcoastBank/Service.Test/AccountTest.cs
@@ -1,8 +1,8 @@
 using Application.Services;
-using Domain.Entities;
 using Moq;
 using Services.DTOs;
 using Services.Interfaces;
+using Xunit.Abstractions;
 
 namespace Service.Test;
 
@@ -10,14 +10,16 @@ public class AccountTest
 {
     private Mock<IAccountRepository> _mockRepo;
     private AccountService _accountService;
+    private readonly ITestOutputHelper _testOutputHelper;
 
-    public AccountTest()
+    public AccountTest(ITestOutputHelper testOutputHelper)
     {
+        _testOutputHelper = testOutputHelper;
         _mockRepo = new Mock<IAccountRepository>();
         _accountService = new AccountService(_mockRepo.Object);
     }
     [Fact]
-    public async Task ListAllAccounts_ShouldReturn_AllAccounts()
+    public async Task ListAllAccounts_ShouldNotReturnNull_WhenAccountsExists()
     {
         // Arrange...
         var accounts = new List<AccountDto>();
@@ -29,4 +31,40 @@ public class AccountTest
         // Assert...
         Assert.NotNull(result);
     }
+
+    [Fact]
+    public async Task ListAllAccountsByCustomerName_ShouldReturnNull_WhenNoAccountsExists()
+    {
+        // Arrange...
+        var accounts = new List<AccountDto>();
+
+        _mockRepo.Setup(repo => repo.ListAllAccounts("Michael Gustavsson")).ReturnsAsync(accounts);
+
+        // Act...
+        var result = await _accountService.ListAllAccounts("Michael Gustavsson");
+
+        // Assert...
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public async Task ListAllAccountsByCustomerName_ShouldNotReturnNull_WhenAccountsExists()
+    {
+        var accounts = new List<AccountDto>
+        {
+            new() { CreatedDate = DateTime.Now, AccountNumber = "111-222222", AccountType = "Basic", Balance = 0.00M, Owner = "Michael Gustavsson" },
+            new() { CreatedDate = DateTime.Now, AccountNumber = "111-333333", AccountType = "Savings", Balance = 5000.00M, Owner = "Michael Gustavsson" }
+        };
+
+        _mockRepo.Setup(repo => repo.ListAllAccounts("Mikael Gustavsson")).ReturnsAsync(accounts);
+
+        var result = await _accountService.ListAllAccounts("Mikael Gustavsson");
+        _testOutputHelper.WriteLine($"Antal konton funna: {result.Count}");
+
+        result.ForEach(a => _testOutputHelper.WriteLine(a.AccountNumber));
+
+        Assert.NotNull(result);
+    }
+
+
 }
